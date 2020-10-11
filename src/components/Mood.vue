18e5cb5d7d2661acf0e08896c6ded28bea05725f
Jordy Pereira <jordy.pereira@prorenata.se>
2020-10-11 14:12:20 +0200

    Add mood component

---
+++ src/components/Mood.vue
@@ -0,0 +1,14 @@
+<template>
+  <button>{{ title }}</button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Mood",
+  props: {
+    title: String,
+  },
+});
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,38 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="flex">
+    <h1>Please tell me your mood today</h1>
+    <Mood
+      v-for="mood in moods"
+      :key="mood"
+      :title="mood"
+      @click="chosenMood = mood"
+    />
+
+    <p>Your mood for {{ today }} is: {{ chosenMood }}</p>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { defineComponent } from "vue";
+import Mood from "@/components/Mood.vue"; // @ is an alias to /src
 
 export default defineComponent({
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld,
+    Mood,
+  },
+  data() {
+    return {
+      moods: ["Happy", "So-so", "Sad"],
+      chosenMood: "",
+    };
+  },
+  computed: {
+    today() {
+      const today = new Date();
+
+      return `${today.getDate()}/${today.getMonth()}/${today.getFullYear()}`;
+    },
   },
 });
 </script>
